be0ef1001a0dbbaeafcc37c8dee8ee3e29277724
Frenzel1337 <31428495+Frenzel1337@users.noreply.github.com>
2018-02-06 17:40:13 +1000

    Change Versioning

--- src/version.h
+++ src/version.h
@@ -36,7 +36,7 @@ static const int PROTOCOL_VERSION = 60011;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60011;
+static const int MIN_PEER_PROTO_VERSION = 60010;
 
 // minimum peer version accepted by DarkSendPool
 static const int MIN_POOL_PEER_PROTO_VERSION = 60011; 
@@ -52,7 +52,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 0;
-static const int NOBLKS_VERSION_END = 60011;
+static const int NOBLKS_VERSION_END = 60010;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
