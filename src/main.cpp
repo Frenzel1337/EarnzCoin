6aeea2f6ce4bf9881c84fca580683672bb2be7a8
Frenzel1337 <31428495+Frenzel1337@users.noreply.github.com>
2018-02-06 12:53:45 +1000

    MN/PoS Reward Increase/Decrease Phase Test #1

--- src/main.cpp
+++ src/main.cpp
@@ -1370,9 +1370,9 @@ int64_t GetProofOfStakeReward(const CBlockIndex* pindexPrev, int64_t nCoinAge, i
 {
     int64_t nSubsidy = 2 * COIN;
     if ((pindexPrev->nHeight > 10) && (pindexPrev->nHeight < 150)) nSubsidy = 40 * COIN;
-    } else if ((pindexPrev->nHeight > 150) && (pindexPrev->nHeight < 200)) nSubsidy = 20 * COIN;
-    } else if ((pindexPrev->nHeight > 200) && (pindexPrev->nHeight < 250)) nSubsidy = 10 * COIN;
-    } else if ((pindexPrev->nHeight > 250) && (pindexPrev->nHeight < 300)) nSubsidy = 5 * COIN;
+    else if ((pindexPrev->nHeight > 150) && (pindexPrev->nHeight < 200)) nSubsidy = 20 * COIN;
+    else if ((pindexPrev->nHeight > 200) && (pindexPrev->nHeight < 250)) nSubsidy = 10 * COIN;
+    else if ((pindexPrev->nHeight > 250) && (pindexPrev->nHeight < 300)) nSubsidy = 5 * COIN;
     return nSubsidy + nFees;
 }
 
