8eb2530577fa2dc7144c76a144cd946d0fece4ad
Frenzel1337 <31428495+Frenzel1337@users.noreply.github.com>
2018-02-07 00:05:51 +0000

    Reactivate Seed Nodes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -134,8 +134,8 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        vSeeds.push_back(CDNSSeedData("",""));
-        vSeeds.push_back(CDNSSeedData("",""));
+        vSeeds.push_back(CDNSSeedData("0","91.121.74.198"));
+        vSeeds.push_back(CDNSSeedData("1","176.31.103.149"));
 
         convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort);
 
