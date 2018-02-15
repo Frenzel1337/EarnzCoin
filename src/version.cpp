3831f51d40e7e308201a11765278738c6e962f48
Frenzel1337 <31428495+Frenzel1337@users.noreply.github.com>
2018-02-15 23:47:18 +1000

    Update Version Build

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("EarnzCoin");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "60010"
+#    define GIT_COMMIT_ID "60011"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
